8b212340ff4a9828a8ab804f13399fc44bbc9919
Andre-PC\Andre <andre.lichtenthaeler@gmail.com>
2018-09-11 19:11:22 +0200

    Closes #17

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,31 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var pump = require('pump');
+var lessPluginCleanCSS = require('less-plugin-clean-css');
+
+var cleanCSSPlugin = new lessPluginCleanCSS({
+	advanced: true
+});
 
 var fileName = "gradient";
 var folderSource = "src/";
 var folderDestination = "dist/";
 var folderDestinationDocs = "docs/assets/js/";
+
+gulp.task("build-less", function(callback) {
+	pump([
+		gulp.src(folderSource + "app/main.less"),
+		less({
+			plugins: [
+				cleanCSSPlugin
+			]
+		}),
+		concat("main.min.css"),
+		gulp.dest("docs/assets/styles/")
+	], callback);
+});
 
 gulp.task("build-js", function (callback) {
 	pump([
@@ -18,4 +37,7 @@ gulp.task("build-js", function (callback) {
 	], callback);
 });
 
-gulp.task("default", ["build-js"]);
+gulp.task("default", [
+	"build-less",
+	"build-js"
+]);
