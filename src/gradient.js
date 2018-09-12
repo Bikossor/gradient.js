ea154b473b00a1df43a2c931f9e082266cdb3c70
Bikossor <andre.lichtenthaeler@gmail.com>
2018-09-12 12:46:30 +0200

    Closes #1

--- src/gradient.js
+++ src/gradient.js
@@ -6,6 +6,16 @@ var gradient = (function() {
         lightness: 0,
         angle: 0
     };
+
+    var eventType = "input";
+    var _options = {};
+
+    function gradient(options) {
+        _options = options;
+
+        gradient.prototype.callback = _options.callback;
+        registerEvents(_options.inputs);
+    };
     
     function inputHueChanged(e) {
         result.hue = e.target.value;
@@ -33,39 +43,58 @@ var gradient = (function() {
     };
     
     function registerEvents(inputElements) {
-        for(var i in inputElements) {
+        for (var i in inputElements) {
             var inputElement = document.querySelector(inputElements[i]);
-                        
+            
             switch(i) {
                 case "hue":
                     inputElement.addEventListener(eventType, inputHueChanged, false);
-                break;
+                    break;
                 case "hueDistance":
                     inputElement.addEventListener(eventType, inputHueDistanceChanged, false);
-                break;
+                    break;
                 case "saturation":
                     inputElement.addEventListener(eventType, inputSaturationChanged, false);
-                break;
+                    break;
                 case "lightness":
                     inputElement.addEventListener(eventType, inputLightnessChanged, false);
-                break;
+                    break;
                 case "angle":
                     inputElement.addEventListener(eventType, inputAngleChanged, false);
-                break;
+                    break;
                 default:
                     throw new Error("Type not found!");
-                break;
+                    break;
             }
         }
     };
 
-    function gradient(options) {
-        gradient.prototype.callback = options.callback;
-        registerEvents(options.inputs);
-    };
-
     gradient.prototype.init = function() {
-        //TODO
+        for (var i in _options.inputs) {
+            var inputElement = document.querySelector(_options.inputs[i]);
+            
+            switch(i) {
+                case "hue":
+                    result.hue = inputElement.value;
+                    break;
+                case "hueDistance":
+                    result.hueDistance = inputElement.value;
+                    break;
+                case "saturation":
+                    result.saturation = inputElement.value;
+                    break;
+                case "lightness":
+                    result.lightness = inputElement.value;
+                    break;
+                case "angle":
+                    result.angle = inputElement.value;
+                    break;
+                default:
+                    throw new Error("Type not found!");
+                    break;
+            }
+        }
+        gradient.prototype.callback(result);
     };
     
     return gradient;
